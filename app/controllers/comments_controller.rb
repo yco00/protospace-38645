2fddbd69745f923ae3a8fd922b8e5ee3ae53f3e4
yco00 <ingo.y.co@gmail.com>
2023-03-07 00:13:51 +0900

    修正_フォームを空のまま「送信する」ボタンを押すとエラーページに遷移

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,7 @@ class CommentsController < ApplicationController
       redirect_to prototype_path(@comment.prototype)
     else
       @prototype = @comment.prototype
-      @comments = @prototype.comments
+      @comments = @prototype.comment
       render "prototypes/show"
     end
   end
